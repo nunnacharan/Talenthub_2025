c355c74955071f1b1e805a54026da3e7f5ca75b6
Nunna Charan Naga Lakshmi Narayana <109160426+nunnacharan@users.noreply.github.com>
2025-01-14 17:13:05 +0530

    Update Login.jsx

--- onevector-frontend/src/components/Login.jsx
+++ onevector-frontend/src/components/Login.jsx
@@ -21,7 +21,7 @@ function Login() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('https://5q5faxzgb7.execute-api.ap-south-1.amazonaws.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
